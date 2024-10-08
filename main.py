286625fe3e58e6b1cd8f07c13de9ebe94ba899e2
JMag <146037185+j-m-a-g@users.noreply.github.com>
2024-10-08 17:55:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from inference.core.interfaces.stream.sinks import render_boxes
 
 # initialize a pipeline object
 pipeline = InferencePipeline.init(
-    model_id="face-emotion-recog/1", # Roboflow model to use
-    video_reference=0, # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
-    on_prediction=render_boxes, # Function to run after each prediction
+    model_id = "face-emotion-recog/1", # Roboflow model
+    video_reference = 0, # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
+    on_prediction = render_boxes
 )
 pipeline.start()
 pipeline.join()
