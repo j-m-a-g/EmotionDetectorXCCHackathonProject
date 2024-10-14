4987462515b6b121fed3cc4d0d72422bf5311725
j-m-a-g <jmaghamez@gmail.com>
2024-10-14 13:22:26 -0400

    Implemented a user interface in Python's tkinter library and added the creation of a separate thread for the webcam stream pipeline to the pipeline_start_join function

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+from tkinter import *
+from tkinter import ttk
+
 from inference import InferencePipeline
 from inference.core.interfaces.stream.sinks import render_boxes
 
-# initialize a pipeline object
+from threading import Thread
+
+# Initialize a pipeline object
 pipeline = InferencePipeline.init(
     model_id = "face-emotion-recog/1", # Roboflow model
     video_reference = 0, # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
     on_prediction = render_boxes
 )
-pipeline.start()
-pipeline.join()
+
+mainWindow = Tk()
+mainWindow.title("Emotion Detector")
+mainWindow.geometry("300x130")
+
+howManySecondsToStayLabel = ttk.Label(mainWindow, text = " Once you click, \"Launch\", in order to exit the current\n session, you must close the app's running Python\n console or terminal window. Alternatively, you can\n attempt to resize the \"Emotion Detector\" window to\n intentionally crash it as well.")
+howManySecondsToStayLabel.pack(side = TOP, pady = 5)
+
+# Function to simultaneously start and join the webcam video stream
+def pipeline_start_join():
+    # Create a separate thread for the video stream pipeline to execute on
+    pipelineThread = Thread(target=pipeline.start())
+    pipelineThread.start()
+
+launchButton = ttk.Button(mainWindow, text = "Launch Session", command = pipeline_start_join)
+launchButton.pack(side = TOP)
+
+mainWindow.mainloop()
